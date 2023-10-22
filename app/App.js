88a6a4852fb99379fb126c298d493f440cd608bb
shreyasuratran <shreya.suratran@gmail.com>
2023-10-22 18:41:29 -0500

    With home page

--- app/App.js
+++ app/App.js
@@ -1,8 +1,6 @@
-/*import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {db, auth} from "./Firebase.js"
 
 export default function App() {
   return (
@@ -20,44 +18,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});*/
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
-function Feed() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Feed Screen</Text>
-        </View>
-    );
-}
-
-function Article() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Article Screen</Text>
-        </View>
-    );
-}
-
-const Drawer = createDrawerNavigator();
-
-function MyDrawer() {
-    return (
-        <Drawer.Navigator useLegacyImplementation>
-            <Drawer.Screen name="Feed" component={Feed} />
-            <Drawer.Screen name="Article" component={Article} />
-        </Drawer.Navigator>
-    );
-}
-
-export default function App() {
-    return (
-        <NavigationContainer>
-            <MyDrawer />
-        </NavigationContainer>
-    );
-}
+});

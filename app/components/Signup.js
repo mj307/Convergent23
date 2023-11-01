5501d5fd40a3a771a30ebf43e773b343e7bdc12d
MJ <73719779+mj307@users.noreply.github.com>
2023-10-31 22:06:47 -0500

    Button

--- app/components/Signup.js
+++ app/components/Signup.js
@@ -1,12 +1,23 @@
 import {StyleSheet, Text, View} from "react-native";
 import React from 'react'
+import { Button } from 'react-native'
 import Menu from "./Menu";
+
 
 const Signup = () => {
     return (
         <View style={styles.menuStyle}>
             <View style={styles.lineStyle}></View>
             <Menu />
+            <Text style={styles.baseText1}>This is temp</Text>
+            <Button style={styles.button}
+  
+                // Some properties given to Button 
+                title="Hello"
+                onPress={() => Alert.alert( 
+                    'Sign Up!')} 
+            /> 
+
             <View
                 style={[
                     styles.lineStyle,
@@ -25,6 +36,14 @@ const styles = StyleSheet.create({
     innerText: {
         color: 'red',
     },
+    button: {
+        color: "pink",
+    },
+    baseText1: {
+        fontWeight: 'bold',
+        fontSize: 18, // You can set the font size
+        color: 'blue', // You can set the text color
+      },
 });
 
 export default Signup;

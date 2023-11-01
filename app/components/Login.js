b11bfa17da8367658fccb21d93f9e5415c9384df
kerrynx <kerrynxu@utexas.edu>
2023-10-31 22:15:18 -0500

    "trying to do login"

--- app/components/Login.js
+++ app/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Menu from "./Menu";
 
+
 const Login = () => {
   const [classCode, setClassCode] = useState('');
   const [username, setUsername] = useState('');
--- app/components/Signup.js
+++ app/components/Signup.js
@@ -1,33 +1,55 @@
+import React, {useState} from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import { auth } from '../firebase';
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import {StyleSheet, Text, View} from "react-native";
-import React from 'react'
-import { Button } from 'react-native'
 import Menu from "./Menu";
-
-
+ 
 const Signup = () => {
-    return (
-        <View style={styles.menuStyle}>
-            <View style={styles.lineStyle}></View>
-            <Menu />
-            <Text style={styles.baseText1}>This is temp</Text>
-            <Button style={styles.button}
-  
-                // Some properties given to Button 
-                title="Hello"
-                onPress={() => Alert.alert( 
-                    'Sign Up!')} 
-            /> 
-
-            <View
-                style={[
-                    styles.lineStyle,
-                    {
-                        marginVertical: 15,
-                    },
-                ]}></View>
+    const navigate = useNavigate();
+ 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+ 
+    const onSubmit = async (e) => {
+      e.preventDefault()
+     
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user);
+            navigate("/login")
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            // ..
+        });
+ 
+   
+    }
+ 
+  return (
+    <View style={styles.menuStyle}>
+        <View style={styles.lineStyle}>
+             <Menu />
+             <View
+                 style={[
+                     styles.lineStyle,
+                     {
+                         marginVertical: 15,
+                     },
+                 ]}></View>
         </View>
-    );
-};
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
     baseText: {
@@ -45,6 +67,28 @@ const styles = StyleSheet.create({
         color: 'blue', // You can set the text color
       },
 });
+ 
+export default Signup
 
-export default Signup;
+
+
+
+// const Signup = () => {
+//     return (
+//         <View style={styles.menuStyle}>
+//             <View style={styles.lineStyle}></View>
+//             <Menu />
+//             <View
+//                 style={[
+//                     styles.lineStyle,
+//                     {
+//                         marginVertical: 15,
+//                     },
+//                 ]}></View>
+//         </View>
+//     );
+// };
+
+
+
 

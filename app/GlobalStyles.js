cb76fbf4df666f20591c7a0880a206bdb0cbe815
yangmichaely <yangmichaely@gmail.com>
2023-12-02 22:12:12 -0600

    quiz and chat

---
+++ app/GlobalStyles.js
@@ -0,0 +1,39 @@
+/* fonts */
+export const FontFamily = {
+  cabinMedium: "Cabin-Medium",
+  cabinMediumItalic: "Cabin-MediumItalic",
+};
+/* font sizes */
+export const FontSize = {
+  size_3xs: 10,
+  size_lg: 18,
+  size_31xl: 50,
+  size_5xs: 8,
+  size_xl: 20,
+  size_xs: 12,
+  size_21xl: 40,
+  size_11xl: 30,
+  size_81xl: 100,
+  size_mini: 15,
+  size_7xs: 6,
+  size_sm: 14,
+};
+/* Colors */
+export const Color = {
+  colorRoyalblue: "#4e75eb",
+  colorDarkgray_100: "#b3b3b3",
+  colorDarkgray_200: "#a3a3a3",
+  colorDarkgray_300: "#9c9c9c",
+  colorWhite: "#fff",
+  colorWhitesmoke: "#f4f4f4",
+  colorDarkslategray: "#434343",
+  colorDimgray_100: "#666",
+  colorDimgray_200: "#5e5e5e",
+  colorGray: "#fbfdff",
+};
+/* border radiuses */
+export const Border = {
+  br_3xs: 10,
+  br_11xl: 30,
+  br_8xs: 5,
+};
